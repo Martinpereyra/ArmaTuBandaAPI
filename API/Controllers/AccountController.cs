fbe1a67dc3780ec8fc95e04bc230074c545b8c49
Martin Pereyra <mapereyra@frro.utn.edu.ar>
2022-05-08 04:31:48 -0300

    Feat: Autenticacion por JWT y deploy services

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,13 +16,15 @@ namespace API.Controllers;
 public class AccountController : BaseApiController
 {
     private readonly DataContext _context;
-    public AccountController(DataContext context)
+    private readonly ITokenService _tokenService;
+    public AccountController(DataContext context, ITokenService tokenService)
     {
+        _tokenService = tokenService;
         _context = context;
     }
 
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto){
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto){
 
         if(await UserExists(registerDto.Username)) return BadRequest("El nombre de usuario ya existe");
 
@@ -36,11 +39,14 @@ public class AccountController : BaseApiController
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
 
-        return user; 
+        return new UserDto{
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
     }
 
     [HttpPost("login")]
-    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto){
         var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
         if (user == null) return Unauthorized("Nombre de usuario invalido");
@@ -53,7 +59,10 @@ public class AccountController : BaseApiController
             if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Contraseña incorrecta");
         }
 
-        return user;
+        return new UserDto{
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
 
 
         
